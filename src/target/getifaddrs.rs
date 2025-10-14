4e08577c75c671880a73b9efefe8691220953e16
Will Chandler <will@oxidecomputer.com>
2025-10-14 14:54:09 -0400

    Fix IfAddrIterator skipping the first interface
    The constructor for `IfAddrIterator` sets the `next` address as the base
    address returned by `getifaddrs(3)`. However, when calling `next` we
    immediately dereference its `ifa_next` member and return that, skipping
    the base item.
    
    Fix this to include the initial interface in the iterator.

--- src/target/getifaddrs.rs
+++ src/target/getifaddrs.rs
@@ -14,13 +14,10 @@ impl Iterator for IfAddrIterator {
             return None;
         }
 
-        let next = unsafe { (*self.next).ifa_next };
-        if next.is_null() {
-            None
-        } else {
-            self.next = next;
-            Some(next)
-        }
+        let current_next = self.next;
+        let next_next = unsafe { (*self.next).ifa_next };
+        self.next = next_next;
+        Some(current_next)
     }
 }
 
@@ -56,5 +53,31 @@ mod tests {
         };
 
         assert_eq!(empty.count(), 0);
+    }
+
+    #[test]
+    fn test_iter_has_all_ifa() {
+        // Get the number of interfaces returned by the `IfAddrIterator` wrapper.
+        let iter_ct = getifaddrs().unwrap().count();
+
+        // Manually get the number of interfaces returned by libc.
+        let mut raw_ct = 0;
+        let mut ifap = mem::MaybeUninit::<*mut libc::ifaddrs>::uninit();
+        let ret = unsafe { libc::getifaddrs(ifap.as_mut_ptr()) };
+
+        assert_eq!(ret, 0);
+        let ifap = unsafe { ifap.assume_init() };
+
+        if !ifap.is_null() {
+            raw_ct += 1;
+        }
+        let mut next = unsafe { (*ifap).ifa_next };
+        while !next.is_null() {
+            raw_ct += 1;
+            next = unsafe { (*next).ifa_next };
+        }
+        unsafe { libc::freeifaddrs(ifap) };
+
+        assert_eq!(iter_ct, raw_ct);
     }
 }
